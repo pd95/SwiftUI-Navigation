00bbd441624cd4c869afbc5b5cfc25d40ecd7153
Philipp <pd95@users.noreply.github.com>
2020-12-10 10:22:58 +0100

    Initial Commit

---
+++ SwiftUI-Navigation/SwiftUI_NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_NavigationApp.swift
+//  SwiftUI-Navigation
+//
+//  Created by Philipp on 10.12.20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
